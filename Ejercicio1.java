3bb076acc92ea39f9a4d05f50d13ff9c2f4d93bf
jotaeme890 <permanentjavi@gmail.com>
2022-10-05 12:27:01 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,7 +1,7 @@
-/*This program is going to write in the screen the text "Hello World" */
+/*This program is going to write in the screen "Javier Miguel Martín" */
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.print("Hello World");  
+        System.out.print("Javier Miguel Martín");  
     }
 }
