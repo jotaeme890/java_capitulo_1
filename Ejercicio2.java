a8523257c85cbda376b03b727245f9222c1e18ae
jotaeme890 <permanentjavi@gmail.com>
2022-10-05 12:32:00 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,7 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Javier Miguel Martín"); 
+        System.out.println("Dirección: Calle Frederick Terman, 3, 29590 Málaga");
+        System.out.println("Número: 000000000"); 
+    }
+}
