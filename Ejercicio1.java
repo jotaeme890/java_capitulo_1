d9785099ebff683bd2e30b52e92e7f21801bd921
jotaeme890 <permanentjavi@gmail.com>
2022-10-05 12:11:55 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,3 +1,5 @@
+/*This program is going to write in the screen the text "Hello World" */
+
 public class Ejercicio1 {
     public static void main(String[] args) {
         System.out.print("Hello World");  
