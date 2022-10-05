c49738408fc995337570bbde1f88a72231c9c33e
jotaeme890 <permanentjavi@gmail.com>
2022-10-05 13:07:30 +0200

    Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,14 @@
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        System.out.printf("%-10s     %-10s \n","Gato","Cat"); 
+        System.out.printf("%-10s     %-10s \n","Pedro","Dog");
+        System.out.printf("%-10s     %-10s \n","Leon","Lion");
+        System.out.printf("%-10s     %-10s \n","Pez","Fish");
+        System.out.printf("%-10s     %-10s \n","Mono","Monkey");
+        System.out.printf("%-10s     %-10s \n","Tiger","Tiger");
+        System.out.printf("%-10s     %-10s \n","Mosquito","Mosquito");
+        System.out.printf("%-10s     %-10s \n","Jirafa","Giraffe");
+        System.out.printf("%-10s     %-10s \n","Ñu","Wildebeest");
+        System.out.printf("%-10s     %-10s \n","Leopardo","Leopard");
+    }
+}
