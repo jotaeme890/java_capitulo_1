e9a67e28125607f49cc0ec020f7326173a67de89
jotaeme890 <permanentjavi@gmail.com>
2022-10-06 09:33:34 +0200

    Ejercicio9

---
+++ Ejercicio9.java
@@ -0,0 +1,8 @@
+public class Ejercicio9 {
+    public static void main(String args[]){
+        System.out.println("  ______");
+        System.out.println(" /|_||_\\`.__");
+        System.out.println("(   _    _ _\\");
+        System.out.println("=`-(_)--(_)-'");
+    }
+}
