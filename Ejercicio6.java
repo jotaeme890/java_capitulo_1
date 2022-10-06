3cf38cce94f5f46b214c9dd83aa8932641aedfda
jotaeme890 <permanentjavi@gmail.com>
2022-10-06 09:22:19 +0200

    Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,7 +1,7 @@
 public class Ejercicio6 {
     public static void main(String args[]){
         System.out.println("    *    ");
-        System.out.println("   ***    ");
+        System.out.println("   ***   ");
         System.out.println("  *****  ");
         System.out.println(" ******* ");
         System.out.println("*********");
