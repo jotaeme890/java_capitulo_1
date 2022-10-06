19797aead2f81245b4b4726d80573bbc8905be99
jotaeme890 <permanentjavi@gmail.com>
2022-10-06 09:37:18 +0200

    Ejercicio10

---
+++ Ejercicio10.java
@@ -0,0 +1,11 @@
+public class Ejercicio10 {
+    public static void main(String args[]){
+        String rojo = "\033[31m";
+        String negro ="\033[90m";
+        
+        System.out.println(rojo+"  ______");
+        System.out.println(rojo+" /|_||_\\`.__");
+        System.out.println(rojo+"(   _    _ _\\");
+        System.out.println(negro+"=`-(_)--(_)-'");
+    }
+}
