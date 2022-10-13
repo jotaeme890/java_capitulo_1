f2b9d0249d2caa024f348dee54c49538ba0f23b4
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:41:05 +0200

    Ejercicio7

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,3 +1,10 @@
+/**
+ * 7 Igual que el programa anterior, pero esta vez la pirámide estará hueca (se debe ver 
+ * únicamente el contorno hecho con asteriscos).
+ * 
+ *  @author jotaeme
+*/
+
 public class Ejercicio7 {
     public static void main(String args[]){
         System.out.println("    *    ");
