81f66d4ecb1782bbbbad0d9b048ccc1fddf71120
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:40:59 +0200

    Ejercicio6

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -1,3 +1,10 @@
+/**
+ * 6 Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos. 
+ * La base de la pirámide debe estar formada por 9 asteriscos.
+ * 
+ *  @author jotaeme
+*/
+
 public class Ejercicio6 {
     public static void main(String args[]){
         System.out.println("    *    ");
