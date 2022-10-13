78e65ad2bf613121f8b87bd4404478feeed95153
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:40:40 +0200

    Ejercicio3

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,3 +1,10 @@
+/**
+ * 3 Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su correspondiente 
+ * traducción al castellano. Las palabras deben estar distribuidas en dos columnas y alineadas a la izquierda.
+ * 
+ *  @author jotaeme
+*/
+
 public class Ejercicio3 {
     public static void main(String[] args) {
         System.out.printf("%-10s     %-10s \n","Gato","Cat"); 
