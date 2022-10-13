2ad08200f21bcbfe94fedc4a09a5ce5c2772a595
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:41:18 +0200

    Ejercicio8

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,3 +1,9 @@
+/**
+ * 8 Igual que el programa anterior, pero esta vez la pirámide debe aparecer invertida, con el vértice hacia abajo.
+ * 
+ *  @author jotaeme
+*/
+
 public class Ejercicio8 {
     public static void main(String args[]){
         System.out.println("*********");
