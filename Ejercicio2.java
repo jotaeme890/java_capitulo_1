9c9c61d5fd974333874d37983f3a8fb9fe8c76ad
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:40:33 +0200

    Ejercicio2

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,3 +1,10 @@
+/**
+ * 2 Modifica el programa anterior para que además se muestre tu dirección y tu número de teléfono. Asegúrate 
+ * de que los datos se muestran en líneas separadas.
+ * 
+ *  @author jotaeme
+*/
+
 public class Ejercicio2 {
     public static void main(String[] args) {
         System.out.println("Javier Miguel Martín"); 
