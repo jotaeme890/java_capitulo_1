c8d6d2768bfaa891f2af84ed81266e9c51d852b5
jotaeme890 <permanentjavi@gmail.com>
2022-10-13 08:40:23 +0200

    Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,4 +1,8 @@
-/*This program is going to write in the screen "Javier Miguel Martín" */
+/**
+ * 1 Escribe una programa que muestre tu nombre por pantalla.
+ * 
+ *  @author jotaeme
+*/
 
 public class Ejercicio1 {
     public static void main(String[] args) {
